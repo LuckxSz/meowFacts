e02ec0ea4c99fa7591ba88ae99c198cf43e2dc06
xkl <joselucas20048jl@gmail.com>
2024-03-14 20:54:10 -0300

    dark mode / light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useFetch } from "./hooks/useFetch";
 
 export const App = () => {
@@ -5,17 +6,43 @@ export const App = () => {
     "https://meowfacts.herokuapp.com/?lang=por"
   );
 
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div>
+    <div
+      className={`flex justify-center items-center h-screen ${
+        darkMode ? "bg-gray-900 text-white" : "bg-pink-100 text-gray-900"
+      }`}
+    >
+      <button
+        onClick={toggleDarkMode}
+        className={`absolute top-4 right-4 px-4 py-2 rounded-lg ${
+          darkMode
+            ? "bg-white text-gray-900 border border-gray-400 hover:bg-gray-100"
+            : "bg-pink-600 text-white border border-pink-700 hover:bg-pink-700"
+        }`}
+      >
+        {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
       {isFetching && <p>Loading...</p>}
-      <div className="flex justify-center items-center h-screen bg-pink-100">
-        <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
-          <h1 className="text-2xl font-semibold text-pink-600 mb-4">
-            Fatos sobre gatinhos
-          </h1>
-          <div className="text-lg text-gray-700">
-            {facts && <p>{facts.data}</p>}
-          </div>
+      <div
+        className={`shadow-lg max-w-md w-full bg-white rounded-lg p-8 ${
+          darkMode ? "text-white" : "text-gray-800"
+        }`}
+      >
+        <h1
+          className={`text-3xl font-semibold mb-4 ${
+            darkMode ? "text-gray-900" : "text-pink-600"
+          }`}
+        >
+          Fatos sobre gatinhos
+        </h1>
+        <div className={`${darkMode ? "text-gray-900" : "text-gray-700"}`}>
+          {facts && <p>{facts.data}</p>}
         </div>
       </div>
     </div>
