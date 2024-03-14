7401fbb1aa033c1ec25664230e24fd8207602d34
xkl <joselucas20048jl@gmail.com>
2024-03-14 19:17:57 -0300

    if net is low, will apear  "loading"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useFetch } from "./hooks/useFetch";
 
 export const App = () => {
-  const { facts } = useFetch("https://meowfacts.herokuapp.com/?lang=por");
+  const { facts, isFetching } = useFetch(
+    "https://meowfacts.herokuapp.com/?lang=por"
+  );
 
   return (
-    <div className="flex justify-center items-center h-screen bg-pink-100">
-      <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
-        <h1 className="text-2xl font-semibold text-pink-600 mb-4">
-          Fatos sobre gatinhos
-        </h1>
-        <div className="text-lg text-gray-700">
-          {facts && <p>{facts.data}</p>}
+    <div>
+      {isFetching && <p>Loading...</p>}
+      <div className="flex justify-center items-center h-screen bg-pink-100">
+        <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
+          <h1 className="text-2xl font-semibold text-pink-600 mb-4">
+            Fatos sobre gatinhos
+          </h1>
+          <div className="text-lg text-gray-700">
+            {facts && <p>{facts.data}</p>}
+          </div>
         </div>
       </div>
     </div>
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 export const useFetch = (url) => {
   const [facts, setFacts] = useState("");
+  const [isFetching, setIsFetching] = useState("true");
 
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setFacts(response.data);
-    });
+    axios
+      .get(url)
+      .then((response) => {
+        setFacts(response.data);
+      })
+      .finally(() => {
+        setIsFetching(false);
+      });
   }, []);
-  return { facts };
+  return { facts, isFetching };
 };
