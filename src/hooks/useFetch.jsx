1a8231235eb43189a966f2a99b81156a2b6ad357
xkl <joselucas20048jl@gmail.com>
2024-03-14 19:32:21 -0300

    axios.create()

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://meowfacts.herokuapp.com/?lang=por",
+});
+
 export const useFetch = (url) => {
   const [facts, setFacts] = useState("");
   const [isFetching, setIsFetching] = useState("true");
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
+    api
       .get(url)
       .then((response) => {
         setFacts(response.data);
+      })
+      .catch((err) => {
+        setError(err);
       })
       .finally(() => {
         setIsFetching(false);
       });
   }, []);
-  return { facts, isFetching };
+  return { facts, isFetching, error };
 };
